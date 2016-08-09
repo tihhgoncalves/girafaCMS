8bd399829c18bd923549845ffd0d052f4e378fc9
Tihh Gonçalves <tihh.gon@gmail.com>
2016-08-09 16:01:24 -0300

    Tirando /modules de dentro do girafaCMS

--- modules/aplicacoes/admin.plugins.form.php
+++
@@ -1,9 +0,0 @@
-<?
-$form = new nbrAdminForms('sysPlugins');
-
-$form->AddFieldString('Name', 'Nome', 50, 3, null, true, true);
-$form->AddFieldString('Path', 'Diretório', 30, 2, null, true, true);
-$form->AddFieldString('Version', 'Versão', 10, 1, null, true, true);
-
-$form->PrintHTML();
-?>
