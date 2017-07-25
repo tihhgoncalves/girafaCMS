b0500859369718341353fb988628af7319d10d3c
Tihh Gonçalves <tihh.gon@gmail.com>
2017-07-25 11:15:18 -0300

    Release 3.0.1
    Preparando pra liberação...

--- objects/nbr.obj.cms.php
+++ objects/nbr.obj.cms.php
@@ -11,7 +11,7 @@ class nbrCMS
   private $themePath;
   private $themeURL;    
   public $isMobile = false;
-  private $version = '3.0.0';
+  private $version = '3.0.1';
 
   
   function __construct($lang = 'pt-br'){
