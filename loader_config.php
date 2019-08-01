11d458290910eed3e4d6d3267f22de96483a9465
Tihh Gonçalves <tihhgoncalves@gmail.com>
2019-08-01 18:34:28 -0300

    Adicionei localhost.gits e localhost.sites no is_localhost() tmb

--- loader_config.php
+++ loader_config.php
@@ -7,7 +7,7 @@ include(dirname(dirname(__FILE__)) . '/tihh.site.uri.php/load.php');
 $uri = new tihh_URI();
 
 function is_localhost(){
-	return (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']));
+	return (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1', 'localhost.gits', 'localhost.sites']));
 }
 
 
